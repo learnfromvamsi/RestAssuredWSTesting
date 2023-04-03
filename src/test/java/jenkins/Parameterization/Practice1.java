251aa7485eaa0afbf91c9943c59ecfab1d41ba0d
vamsi <113770873+learnvamsi@users.noreply.github.com>
2023-04-03 09:51:06 +0530

    added a file

---
+++ src/test/java/jenkins/Parameterization/Practice1.java
@@ -0,0 +1,11 @@
+package jenkins.Parameterization;
+
+import org.testng.annotations.Test;
+
+public class Practice1 {
+	@Test
+	public void demoTest() {
+		System.out.println(System.getProperty("browser"));
+	}
+
+}
